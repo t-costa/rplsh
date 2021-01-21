0a5a2beada619f536d2c12f69550d8d435607ad6
t-costa <tommasocostanzo93@gmail.com>
2021-01-21 21:59:56 +0100

    debug

--- test/times_test/prova.cpp
+++ test/times_test/prova.cpp
@@ -20,14 +20,13 @@
 // business code headers
 #include </home/tommaso/forked/rplsh/test/definition.hpp>
 
-
+using namespace ff;
+using namespace std;
+using Problem = long;
+using Result  = long;
 
 class source_vec_stage_stage : public ff_node {
-protected:
-	std::unique_ptr<source_vec_stage> src;
-
 public:
-	source_vec_stage_stage() : src(new source_vec_stage()) {}
 	int svc_init() {
 		#ifdef TRACE_CORE
 		std::cout << "svc_init -- source_vec_stage -- id = "		<< get_my_id() << " -- tid = " << std::this_thread::get_id() << " -- core = " << sched_getcpu() << std::endl;
@@ -36,18 +35,14 @@ public:
 	}
 
 	void * svc(void *t) {
-		while( src->has_next() )
-			ff_send_out((void*) src->next() );
+			int* a = new int(20);
+			ff_send_out((void*) a);
 		return (NULL);
 	}
 };
 
 class drain_vec_stage_stage : public ff_node {
-protected:
-	std::unique_ptr<drain_vec_stage> drn;
-
 public:
-	drain_vec_stage_stage() : drn(new drain_vec_stage()) {}
 	int svc_init() {
 		#ifdef TRACE_CORE
 		std::cout << "svc_init -- drain_vec_stage -- id = "		<< get_my_id() << " -- tid = " << std::this_thread::get_id() << " -- core = " << sched_getcpu() << std::endl;
@@ -56,48 +51,62 @@ public:
 	}
 
 	void * svc(void *t) {
-		drn->process((std::vector<utils::elem_type>*) t);
+		delete (int*)t;
 		return (GO_ON);
 	}
 };
+
+
+
 
 int main( int argc, char* argv[] ) {
 	// worker mapping
 	const char worker_mapping[] = "0,1,2";
 	threadMapper::instance()->setMappingList(worker_mapping);
-	int nw = 1;
+	int nw = 2;
 
-	while (nw <= 128) {
+	// while (nw <= 128) {
 		source_vec_stage_stage _source_vec_stage;
-		dc_dummy dc_stage;
-		ff_DC<std::vector<utils::elem_type>, std::vector<utils::elem_type>> _dc0_(
-			[&](const std::vector<utils::elem_type>& in, std::vector<std::vector<utils::elem_type>>& in_vec) {
-				dc_stage.divide(in, in_vec);
-			},
-			[&](std::vector<std::vector<utils::elem_type>>& out_vec, std::vector<utils::elem_type>& out) {
-				dc_stage.combine(out_vec, out);
-			},
-			[&](const std::vector<utils::elem_type>& in, std::vector<utils::elem_type>& out) {
-				dc_stage.seq(in, out);
-			},
-			[&](const std::vector<utils::elem_type>& in) {
-				return dc_stage.cond(in);
-			},
-		nw);
+		// dc_dummy dc_stage;
+		// ff_DC<std::vector<utils::elem_type>, std::vector<utils::elem_type>> _dc0_(
+		// 	[&](const std::vector<utils::elem_type>& in, std::vector<std::vector<utils::elem_type>>& in_vec) {
+		// 		dc_stage.divide(in, in_vec);
+		// 	},
+		// 	[&](std::vector<std::vector<utils::elem_type>>& out_vec, std::vector<utils::elem_type>& out) {
+		// 		dc_stage.combine(out_vec, out);
+		// 	},
+		// 	[&](const std::vector<utils::elem_type>& in, std::vector<utils::elem_type>& out) {
+		// 		dc_stage.seq(in, out);
+		// 	},
+		// 	[&](const std::vector<utils::elem_type>& in) {
+		// 		return dc_stage.cond(in);
+		// 	},
+		// nw);
+		long res;
+
+		ff_DC<long, long> dac(
+	                          [](const Problem &op,std::vector<Problem> &subops){
+	                              subops.push_back(op-1);
+	                              subops.push_back(op-2);
+	                          },
+	                          [](vector<Result>& res, Result &ret){ ret=res[0]+res[1]; },
+	                          [](const Problem &, Result &res)  { res=1; },
+	                          [](const Problem &op){ return (op<=2); },
+	                          nw);
 		drain_vec_stage_stage _drain_vec_stage;
 		ff_pipeline pipe;
 		pipe.add_stage(&_source_vec_stage);
-		pipe.add_stage(&_dc0_);
+		pipe.add_stage(&dac);
 		pipe.add_stage(&_drain_vec_stage);
 
 		pipe.run_and_wait_end();
 		std::cout << "Divide and conquer: nw = " << nw << ". ";
 		std::cout << "Spent: " << pipe.ffTime() << " msecs" << std::endl;
-		if (nw < 32)
-			nw++;
-		else
-			nw *= 2;
-	}
+		// if (nw < 32)
+		// 	nw++;
+		// else
+		// 	nw *= 2;
+	// }
 
 
 
