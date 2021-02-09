d0337fa6698a724f036215a320d43c52fb166e0d
t-costa <tommasocostanzo93@gmail.com>
2021-02-09 19:32:24 +0100

    Cleaning git dir

--- business/dotproduct/build/CMakeFiles/business.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libbusiness.a"
-  "libbusiness.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/business.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
