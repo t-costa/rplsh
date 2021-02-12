5472ed41df466e39061fef5515da4c4fdb9ee875
t-costa <tommasocostanzo93@gmail.com>
2021-02-12 19:23:10 +0100

    update wait

--- test/parameters.hpp
+++ test/parameters.hpp
@@ -15,6 +15,6 @@ namespace parameters {
   const int matrix_production = matrix_size*matrix_size;
   const int cut_off = 200;
 #endif
-  const int minimum_wait = 100;
+  const int minimum_wait = 10;
   bool sequential = true;
 }
